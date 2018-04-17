80b24988757d5c9d4337140ee7518aa8fb83f404
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-17 03:57:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -102,8 +102,9 @@ holiday_hash.each do |season, data|
   end
 end 
 new_array.flatten!
+new_array.map! {|item| item.to_sym}
 
-      puts "new array = #{new_array.map {|item| item.to_sym}}"
+      puts "new array = #{new_array}"
 end
 
 
